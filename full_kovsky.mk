6a3e33e53bc6fbd2463cb8a2982ac1d6902bf22d
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2011-11-13 02:15:22 +0400

    Change default network type to GSM

--- full_kovsky.mk
+++ full_kovsky.mk
@@ -105,7 +105,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Default network type
 # 0 => WCDMA Preferred.
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_network=0
+    ro.telephony.default_network=1
 
 # Disable JIT by default
 PRODUCT_PROPERTY_OVERRIDES += \
